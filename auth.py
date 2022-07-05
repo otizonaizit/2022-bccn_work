c78fa63e689d1f6e6b5ac9df8427b62b245a9c3b
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2022-07-05 12:44:13 +0200

    reading and writing databases

--- auth.py
+++ auth.py
@@ -1,3 +1,5 @@
+import json
+
 def get_credentials():
     username = input('Enter your username: ')
     password = input('Enter your password: ')
@@ -10,6 +12,14 @@ def authenticate(username, password, pwdb):
             status = True
     return status
 
+def read_pwdb(path):
+    with open(path, 'rt') as pwdb_file:
+        pwdb = json.load(pwdb_file)
+    return pwdb
+
+def write_pwdb(pwdb, path):
+    with open(path, 'wt') as pwdb_file:
+        json.dump(pwdb, pwdb_file)
 
 
 if __name__ == "__main__":
@@ -17,7 +27,7 @@ if __name__ == "__main__":
 
     pwdb = {"lisa":"asd"}
     status = authenticate(username, password, pwdb)
-
+    write_pwdb(pwdb, "pwdb.json")
     if status:
         print("success")
     else:
