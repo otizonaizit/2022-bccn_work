dd59da6eadc568489f7848ff487abaab9af7bc6d
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2022-07-05 12:13:54 +0200

    added authentication function

--- auth.py
+++ auth.py
@@ -3,6 +3,22 @@ def get_credentials():
     password = input('Enter your password: ')
     return username, password
 
+def authenticate(username, password, pwdb):
+    status = False
+    if username in pwdb:
+        if password == pwdb[username]:
+            status = True
+    return status
+
+
 
 if __name__ == "__main__":
-    get_credentials()
+    username, password = get_credentials()
+
+    pwdb = {"lisa":"asd"}
+    status = authenticate(username, password, pwdb)
+
+    if status:
+        print("success")
+    else:
+        print("failed")
