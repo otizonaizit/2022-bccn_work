74289d4c7404e31ccca23451c631c3aac93c1904
Tiziano Zito <opossumnano@gmail.com>
2022-07-05 12:41:39 +0200

    added r/w of pw database

--- auth.py
+++ auth.py
@@ -1,3 +1,5 @@
+import json
+
 def get_credentials():
     username = input('Enter your username: ')
     password = input('Enter your password: ')
@@ -10,12 +12,21 @@ def authenticate(username, password, pwdb):
             status = True
     return status
 
+def read_pwdb(filename):
+    with open(filename, 'rt') as pwdb_file:
+        pwdb = json.load(pwdb_file)
+    return pwdb
 
+def write_pwdb(filename, pwdb):
+    with open(filename, 'wt') as pwdb_file:
+        json.dump(pwdb, pwdb_file)
 
 if __name__ == "__main__":
     username, password = get_credentials()
 
-    pwdb = {"lisa":"asd"}
+    #pwdb = {"lisa":"asd"}
+    #write_pwdb('pwdb.json', pwdb)
+    pwdb = read_pwdb('pwdb.json')
     status = authenticate(username, password, pwdb)
 
     if status:
